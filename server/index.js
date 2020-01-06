64c3ffbd85963a0ebb22b82503c763f922e3ec53
yug-shah0106 <57866661+yug-shah0106@users.noreply.github.com>
2020-01-06 19:59:34 +0530

    Read port from environment variable.

--- server/index.js
+++ server/index.js
@@ -29,6 +29,6 @@ app.use(express.static(root));
 app.get("*", (req, res) => {
   res.sendFile('index.html', { root });
 })
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT,console.log(`server started at port ${PORT}`));
